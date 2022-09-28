a923a2774101b0ad1e6335b5660c58c284e66ba1
dragonfly1033 <42915850+dragonfly1033@users.noreply.github.com>
2022-09-28 09:12:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,47 @@
 #!/bin/sh
 
-pacman -S ttf-iosevka-nerd &
+
+micro
+sudo
+grub
+efibootmgr
+dosfstools
+os-prober
+mtools
+networkmanager
+git
+yay
+picom
+picom-jonaburg-git
+xorg
+xorg-xinit
+mlocate
+feh
+alacritty
+xterm
+awesome
+firefox
+lightdm
+lightdm-webkit2-greeter
+lightdm-webkit-theme-litarvan
+rofi
+polybar
+dunst
+cronie
+exa
+tree
+openssh
+pacman-contrib
+gpick
+
+
+
+
+pacman -S ttf-iosevka-nerd ttf-raleway-variable &
 mv bin .. &
-mv .config .. &
-mv fonts/* /usr/local/share/fonts/ &
+cp .config/* ../.config &
+mv -r fonts/* /usr/local/share/fonts/ &
 mv scripts .. &
 mv .themes .. &
-mkdir ~/Pictures ; mv wallpapers ../Pictures &
+mkdir ~/Pictures &
+mv -r wallpapers ../Pictures &
