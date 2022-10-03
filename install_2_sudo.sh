ff5a845beaf7b1fe4ec1fe25b842bce3707ff1ef
Shrey Patel <shreyrg14@gmail.com>
2022-10-03 16:47:41 +0100

    fix typo

--- install_2_sudo.sh
+++ install_2_sudo.sh
@@ -8,5 +8,5 @@ pacman -S --noconfirm ttf-iosevka-nerd neofetch
 sed -i '/#greeter-session/c\greeter-session=lightdm-webkit2-greeter' /etc/lightdm/lightdm.conf
 sed -i '/#width/c\width=1920' /etc/lightdm/lightdm.conf
 sed -i '/#height/c\height=1080' /etc/lightdm/lightdm.conf
-sed -i '/webkit-theme/c\webkit_theme=litarvan' /etc/lightdm/lightdm-webkit2-greeter.conf
+sed -i '/webkit_theme/c\webkit_theme=litarvan' /etc/lightdm/lightdm-webkit2-greeter.conf
 
