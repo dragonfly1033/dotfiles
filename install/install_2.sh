0f57385a0a988fb3269883cf259a93cd200a3367
dragonfly1033 <=>
2024-03-04 11:40:57 +0000

    make yay installs separate

--- install/install_2.sh
+++ install/install_2.sh
@@ -126,11 +126,3 @@ yes '
 cd /home/$username
 rm -rf yay-bin
 
-echo "--------------------------------------------------------------------------------------"
-echo "INSTALL AUR PKGS"
-echo "--------------------------------------------------------------------------------------"
-
-yes '
-' | yay -S --sudoloop --needed --mflags "--noconfirm" $(cat ~/.dotfiles/install/yay_pkgs | xargs)
-
-
