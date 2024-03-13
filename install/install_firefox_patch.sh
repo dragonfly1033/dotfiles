43a907fc9c8dffd0f0ec21972c6340706cdc9c68
dragonfly1033 <=>
2024-03-13 16:02:21 +0000

    fix firefox patch

--- install/install_firefox_patch.sh
+++ install/install_firefox_patch.sh
@@ -6,8 +6,10 @@ for i in $(find ~ -maxdepth 3 -type d -path "/home/$username/.mozilla/firefox/*.
     cp -r ~/.dotfiles/files/firefox/chrome "$i"/chrome
     sed -r "s!XXXXX!file://$i/chrome/resources/userChrome.css!" -i "$i/chrome/userChrome.css"
     cp ~/.dotfiles/files/firefox/user.js "$i"
+    rm -rf "$i"/startupCache/*
 done
 
 # find ~ -maxdepth 3 -type d -path "*/.mozilla/firefox/*.*" -exec cp -r ~/.dotfiles/files/firefox/chrome "{}"/chrome \;
 
 sudo cp ~/.dotfiles/files/firefox/config.js /usr/lib/firefox
+sudo cp ~/.dotfiles/files/firefox/config-prefs.js /usr/lib/firefox/defaults/pref
